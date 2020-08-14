94a8ba7c4ec4447939adbb128eb307cc4cbecc00
Yazan widyan <yazanwidyan1@gmail.com>
2020-08-14 18:01:39 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Chat from "./Containers/Chat/Chat";
+import Login from "./Components/Login/Login";
+import { useStateValue } from "./ContextAPI/StateProvider";
 
 function App() {
+  // const [user, setUser] = useState(null);
+  const [{ user }, dispatch] = useStateValue();
+
+  console.log(user);
+
   return (
     <div className="App">
       <Router>
-        <Header />
-        <div className="app__body">
-          <Sidebar />
-          <Switch>
-            <Route path="/room/:roomId">
-              <Chat />
-            </Route>
-            <Route path="/">
-              <h1>welcome</h1>
-            </Route>
-          </Switch>
-        </div>
+        {!user ? (
+          <Login />
+        ) : (
+          <>
+            <Header />
+            <div className="app__body">
+              <Sidebar />
+              <Switch>
+                <Route path="/room/:roomId">
+                  <Chat />
+                </Route>
+                <Route path="/">
+                  <h1>welcome</h1>
+                </Route>
+              </Switch>
+            </div>
+          </>
+        )}
       </Router>
     </div>
   );
