5eb2a08f3d8ac1b713d35f12ea9cfbd035f08ea7
Yazan widyan <yazanwidyan1@gmail.com>
2020-08-14 15:20:10 +0300

    modify

--- src/App.js
+++ src/App.js
@@ -3,14 +3,25 @@ import "./App.css";
 import Header from "./Components/Header/Header";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Chat from "./Containers/Chat/Chat";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <div className="app__body">
-        <Sidebar />
-      </div>
+      <Router>
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+          <Switch>
+            <Route path="/room/:roomId">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <h1>welcome</h1>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
